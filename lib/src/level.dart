c534d6a7c002c6cf685437aab28a152d05e3c3e3
vxern <vxern@wordcollector.co.uk>
2021-08-20 23:03:07 +0100

    Formatted in accordance with `dartfmt`

--- lib/src/level.dart
+++ lib/src/level.dart
@@ -2,14 +2,19 @@
 enum Level {
   /// Gray; Only for the developer to see.
   debug,
+
   /// Green; Indicative of a good reason for celebration
   success,
+
   /// Cyan; Normal flow of execution inside the code
   info,
+
   /// Yellow; What happened is bordering on being unacceptable
   warn,
+
   /// Red; The task must be aborted, but the component can keep functioning
   severe,
+
   /// Red on yellow background; The task must be aborted, and the component
   /// cannot keep functioning
   fatal,
--- lib/src/sprint.dart
+++ lib/src/sprint.dart
@@ -9,6 +9,7 @@ class Sprint {
 
   /// When set to `true`, debug messages will not be displayed
   final bool productionMode;
+
   /// When set to `true`, no messages will be displayed
   bool quietMode;
 
@@ -16,23 +17,24 @@ class Sprint {
   final bool includeTimestamp;
 
   /// Create an instance of `Sprint`, indicated as belonging to [owner]
-  /// 
+  ///
   /// [includeTimestamp] - Whether to include a timestamp with the message
-  /// 
+  ///
   /// [productionMode] - Whether the project using `Sprint` is in production
   /// mode, thus stopping the printing of `Severity.debug` messages
-  /// 
+  ///
   /// [quietMode] - Whether the printing of messages should be stopped entirely
-  Sprint(this.owner, {
+  Sprint(
+    this.owner, {
     this.includeTimestamp = false,
     this.productionMode = false,
-    this.quietMode = false, 
+    this.quietMode = false,
   });
 
-  ///
+  /// Obtains a timestamp if [includeTimestamp] is `true`
   String get timestamp => includeTimestamp ? ' ${DateTime.now()}' : '';
 
-  /// Prints the message in the colour determined by the log level 
+  /// Prints the message in the colour determined by the log level
   void log(dynamic message, {Level level = Level.info}) {
     if (quietMode) {
       return;
@@ -56,14 +58,15 @@ class Sprint {
         pen.red();
         break;
       case Level.fatal:
-        pen..red()..yellow(bg: true);
+        pen
+          ..red()
+          ..yellow(bg: true);
         break;
     }
 
     // Keep indentation on-level with the owner string
-    final indented = message
-      .toString()
-      .replaceAll('\n', '\n' ' ' * (3 + owner.length));
+    final indented =
+        message.toString().replaceAll('\n', '\n' ' ' * (3 + owner.length));
     final formatted = '[$owner]$timestamp $indented';
 
     print(pen(formatted));
@@ -71,35 +74,43 @@ class Sprint {
 
   /// Prints a debug message
   void debug(dynamic message) => log(message, level: Level.debug);
+
   /// Alias for `debug()`
   void d(dynamic message) => debug(message);
 
   /// Prints a success message
   void success(dynamic message) => log(message, level: Level.success);
+
   /// Alias for `success()`
   void s(dynamic message) => success(message);
 
   /// Prints an informational message
   void info(dynamic message) => log(message);
+
   /// Alias for `info()`
   void information(dynamic message) => info(message);
+
   /// Alias for `info()`
   void i(dynamic message) => info(message);
 
   /// Prints a warning message
   void warn(dynamic message) => log(message, level: Level.warn);
+
   /// Alias for `warn()`
   void warning(dynamic message) => warn(message);
+
   /// Alias for `warn()`
   void w(dynamic message) => warn(message);
 
   /// Prints a severe message
   void severe(dynamic message) => log(message, level: Level.severe);
+
   /// Alias for `severe()`
   void sv(dynamic message) => severe(message);
 
   /// Prints a fatal message
   void fatal(dynamic message) => log(message, level: Level.fatal);
+
   /// Alias for `fatal()`
   void f(dynamic message) => fatal(message);
 
