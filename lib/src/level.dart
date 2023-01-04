a1aae278a03866718def37ea56b97ea30dd1a96d
vxern <vxern@wordcollector.co.uk>
2023-01-04 21:02:04 +0000

    misc: Maintability and readability changes.

--- lib/src/level.dart
+++ lib/src/level.dart
@@ -1,21 +1,39 @@
-/// Determines the severity of a particular message.
+/// Determines the importance of a particular log message.
+// The `level` is explicitly specified so as to not rely on something as
+// dynamic as the index of an enum value, as well as to be able to specify
+// log levels with the same `level`.
 enum Level {
-  /// Gray; Only for the developer to see.
-  debug,
+  /// Describes a message only for the developer to see.
+  debug(0),
 
-  /// Green; Indicative of a good reason for celebration.
-  success,
+  /// Describes a message indicative of a good reason for celebration.
+  success(1),
 
-  /// Cyan; Normal flow of execution inside the code.
-  info,
+  /// Describes a message indicative of normal flow of execution inside the
+  /// code.
+  info(2),
 
-  /// Yellow; What happened is bordering on being unacceptable.
-  warn,
+  /// Describes a message indicative of an occurrence that is neither serious
+  /// enough to disturb execution flow nor does it come without any
+  /// repercussions at all.
+  warn(3),
 
-  /// Red; The task must be aborted, but the component can keep functioning.
-  severe,
+  /// Describes a message indicative of an occurrence causing a particular
+  /// component to fail, but not fatal, meaning execution can continue.
+  severe(4),
 
-  /// Red on yellow background; The task must be aborted, and the component
-  /// cannot keep functioning.
-  fatal,
+  /// Describes a message indicative of an occurrence causing a particular
+  /// component or the whole program to not be able to continue functioning.
+  fatal(5);
+
+  @override
+  @Deprecated('Use [level] for comparisons')
+  int get index;
+
+  /// Used to determine whether a message should be logged based on whether it
+  /// is equal to or greater than the specified minimum log level.
+  final int level;
+
+  /// Constructs a `Level`.
+  const Level(this.level);
 }
--- lib/src/sprint.dart
+++ lib/src/sprint.dart
@@ -2,34 +2,43 @@ import 'package:tint/tint.dart';
 
 import 'package:sprint/src/level.dart';
 
-/// Defines the signature of a function responsible for logging to the terminal
-/// or console.
+/// The signature of a function responsible for logging a message.
 typedef LogFunction = void Function(dynamic message, {Level level});
 
-/// Defines the signature of a function responsible for colouring a string.
-typedef ColorFunction = String Function(String string);
+/// The signature of a function responsible for formatting text into a log
+/// message.
+typedef FormattingFunction = String Function(String text);
+
+/// The signature of a function responsible for colouring a message.
+typedef ColorFunction = String Function(String message);
 
 /// Determines whether the program is running in a JavaScript environment.
 const isWeb = identical(0, 0.0);
 
-/// Maps logging levels to the methods that colour them.
-final colorMappings = <Level, ColorFunction>{
-  Level.debug: (string) => string.gray(),
-  Level.success: (string) => string.green(),
-  Level.info: (string) => string.cyan(),
-  Level.warn: (string) => string.yellow(),
-  Level.severe: (string) => string.red(),
-  Level.fatal: (string) => string.red().onYellow(),
-};
+/// Defines the default log level of messages.
+const defaultLogLevel = Level.success;
 
-/// Printing API that allows for simple printing of messages.
-class Sprint {
+/// Maps logging levels to the methods that colour them.
+final colorFunctionByLevel =
+    Map<Level, ColorFunction>.unmodifiable(<Level, ColorFunction>{
+  Level.debug: (message) => message.gray(),
+  Level.success: (message) => message.green(),
+  Level.info: (message) => message.cyan(),
+  Level.warn: (message) => message.yellow(),
+  Level.severe: (message) => message.red(),
+  Level.fatal: (message) => message.red().onYellow(),
+});
+
+/// The base implementation of `Sprint`.
+class _SprintBase {
   /// Name of the module this instance of `Sprint` is being used for logging
   /// messages about.
   final String module;
 
+  final String _indentation;
+
   /// When set to true, debug messages will not be displayed.
-  @Deprecated('Specify a minimum log level instead')
+  @Deprecated('Specify a [minimumLogLevel] instead')
   final bool productionMode;
 
   /// Specifies the minimum severity of messages to log.
@@ -44,23 +53,33 @@ class Sprint {
   /// Prints a message to the web console or to the terminal.
   late final LogFunction log;
 
-  /// Create an instance of `Sprint`, indicated as belonging to [module].
-  ///
-  /// [productionMode] - Whether the project using `Sprint` is in production
-  /// mode, thus stopping the printing of `Severity.debug` messages.
-  ///
-  /// [includeTimestamp] - Whether to include a timestamp with the message.
-  ///
-  /// [quietMode] - Whether the printing of messages should be stopped entirely.
-  Sprint(
+  _SprintBase(
     this.module, {
-    @Deprecated('Specify a minimum log level instead')
-        this.productionMode = false,
-    this.minimumLogLevel = Level.debug,
-    this.includeTimestamp = false,
-    this.quietMode = false,
-  }) {
+    @Deprecated('Specify a [minimumLogLevel] instead')
+        required this.productionMode,
+    required this.minimumLogLevel,
+    required this.includeTimestamp,
+    required this.quietMode,
+  }) : _indentation = ' ' * (3 + module.length) {
     log = isWeb ? _printToConsole : _printToTerminal;
+  }
+
+  void _printToConsole(dynamic message, {Level level = defaultLogLevel}) {
+    if (quietMode || level.level < minimumLogLevel.level) {
+      return;
+    }
+
+    print(format(message));
+  }
+
+  void _printToTerminal(dynamic message, {Level level = defaultLogLevel}) {
+    if (quietMode || level.level < minimumLogLevel.level) {
+      return;
+    }
+
+    final colorMessage = colorFunctionByLevel[level]!;
+
+    print(format(colorMessage(message.toString())));
   }
 
   /// Obtains a timestamp if [includeTimestamp] is `true`.
@@ -68,69 +87,74 @@ class Sprint {
 
   /// Formats a message with a timestamp and the owner of the `Sprint` instance.
   String format(dynamic message) {
-    final content =
-        message.toString().replaceAll('\n', '\n${' ' * (3 + module.length)}');
+    final content = message.toString().replaceAll('\n', '\n$_indentation');
     return '$timestamp<$module> $content';
   }
 
-  void _printToConsole(dynamic message, {Level level = Level.info}) {
-    if (quietMode || level.index < minimumLogLevel.index) {
-      return;
-    }
-
-    print(format(message));
-  }
-
-  void _printToTerminal(dynamic message, {Level level = Level.info}) {
-    if (quietMode || level.index < minimumLogLevel.index) {
-      return;
-    }
-
-    print(format(colorMappings[level]!(message.toString())));
-  }
+  /// A call on the instance itself is synonymous with an `info()` call.
+  void call(dynamic message) => info(message);
 
   /// Prints a debug message.
   void debug(dynamic message) => log(message, level: Level.debug);
 
-  /// Alias for `debug()`.
-  void d(dynamic message) => debug(message);
-
   /// Prints a success message.
   void success(dynamic message) => log(message, level: Level.success);
-
-  /// Alias for `success()`.
-  void s(dynamic message) => success(message);
 
   /// Prints an informational message.
   void info(dynamic message) => log(message);
 
-  /// Alias for `info()`.
-  void information(dynamic message) => info(message);
-
-  /// Alias for `info()`.
-  void i(dynamic message) => info(message);
-
   /// Prints a warning message.
   void warn(dynamic message) => log(message, level: Level.warn);
-
-  /// Alias for `warn()`.
-  void warning(dynamic message) => warn(message);
-
-  /// Alias for `warn()`.
-  void w(dynamic message) => warn(message);
 
   /// Prints a severe message.
   void severe(dynamic message) => log(message, level: Level.severe);
 
-  /// Alias for `severe()`.
-  void sv(dynamic message) => severe(message);
-
   /// Prints a fatal message.
   void fatal(dynamic message) => log(message, level: Level.fatal);
+}
+
+/// Defines aliases for the standard log functions.
+mixin _Aliases on _SprintBase {
+  /// Alias for `debug()`.
+  void d(dynamic message) => debug(message);
+
+  /// Alias for `success()`.
+  void s(dynamic message) => success(message);
+
+  /// Alias for `info()`.
+  void i(dynamic message) => info(message);
+
+  /// Alias for `warn()`.
+  void w(dynamic message) => warn(message);
+
+  /// Alias for `severe()`.
+  void sv(dynamic message) => severe(message);
 
   /// Alias for `fatal()`.
   void f(dynamic message) => fatal(message);
 
-  /// A call on the instance itself is synonymous with an `info()` call.
-  void call(dynamic message) => info(message);
+  /// Alias for `info()`.
+  void information(dynamic message) => info(message);
+
+  /// Alias for `warn()`.
+  void warning(dynamic message) => warn(message);
+}
+
+/// Printing API that allows for simple printing of messages.
+class Sprint extends _SprintBase with _Aliases {
+  /// Create an instance of `Sprint`, indicated as managing logs for [module].
+  ///
+  /// [minimumLogLevel] - The minimum log level to consider when attempting to
+  /// log a message.
+  ///
+  /// [includeTimestamp] - Whether to include a timestamp with the message.
+  ///
+  /// [quietMode] - Whether the printing of messages should be stopped entirely.
+  Sprint(
+    super.module, {
+    super.productionMode = false,
+    super.minimumLogLevel = Level.debug,
+    super.includeTimestamp = false,
+    super.quietMode = false,
+  });
 }
